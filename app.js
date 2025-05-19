2517bb75aea2f3aacf3ed8805b3f5f38d76d65c1
Isa-Muhammad <isamuhammadm.im@gmail.com>
2025-05-19 17:37:27 +0100

    Handle Get events request

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 const express = require("express");
+const fs = require("fs");
 
 const app = express();
+
+const events = JSON.parse(
+  fs.readFileSync(`${__dirname}/data/events.json`, "utf-8")
+);
+
+console.log(events);
+
+app.get("/api/v1/events", (req, res) => {
+  res
+    .status(200)
+    .json({ status: "success", results: events.length, data: { events } });
+});
 
 const port = 3000;
 app.listen(port, "127.0.0.1", () => {
