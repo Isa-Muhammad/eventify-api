a799e9beaadbc7b3a341c9880152e16888839647
Isa-Muhammad <isamuhammadm.im@gmail.com>
2025-05-19 18:10:39 +0100

    Hanlde create new event request

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const fs = require("fs");
 
 const app = express();
 
+// Middleware
+app.use(express.json());
+
 const events = JSON.parse(
   fs.readFileSync(`${__dirname}/data/events.json`, "utf-8")
 );
@@ -13,6 +16,24 @@ app.get("/api/v1/events", (req, res) => {
   res
     .status(200)
     .json({ status: "success", results: events.length, data: { events } });
+});
+
+app.post("/api/v1/events", (req, res) => {
+  console.log(req.body);
+
+  const newId = events[events.length - 1]._id + 1;
+  const newEvent = Object.assign({ _id: newId }, req.body);
+
+  events.push(newEvent);
+
+  fs.writeFile(`${__dirname}/data/events.json`, JSON.stringify(events), () => {
+    res.status(201).json({
+      status: "success",
+      data: {
+        event: newEvent,
+      },
+    });
+  });
 });
 
 const port = 3000;
